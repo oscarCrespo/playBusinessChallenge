e927302080887eb42acf9768d16fbd07cdb0b958
Oscar Crespo <crespo.krw@gmail.com>
2016-06-10 14:07:48 -0500

    base layout ready interaction added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+var s = skrollr.init();
+(function($){
+	
 
+	// Listeners
+	$('.proyectoImg').click(openDetails);
+	$('.followBtn').click(followProyect);
+	$('.proyectosMenu').on('click', '.btn', scrollToSection);
+
+	var $body = $('html, body');
+
+	function openDetails(e) {
+		$('.proyecto').removeClass('active');
+		$('#mainApp').addClass('secondaryPanelActive');
+		$(this).parent().addClass('active');
+	}
+
+	function followProyect(e) {
+		$(this).toggleClass('active');;
+	}
+
+	function scrollToSection(e) {
+		var sectionId = $(this).attr('data-section');
+		var targetSection = $("#"+sectionId);
+
+		$body.animate({
+          scrollTop: targetSection.offset().top - 55
+        }, 500);
+	}
+
+
+})(jQuery)
