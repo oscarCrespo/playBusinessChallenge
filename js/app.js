e996449c299c9e2ee486ce7e1392ef2181a0a27c
Oscar Crespo <crespo.krw@gmail.com>
2016-06-14 02:21:42 -0500

    advanced on right bar

--- js/app.js
+++ js/app.js
@@ -13,14 +13,28 @@ var Project = React.createClass({
 		var index = this.props.index;
 		this.props.followProject(index)
 	},
+	projectClick: function(){
+
+		this.props.onClick(this.props.data.id)
+	},
 	render: function(){
+		var projectClass = 'proyecto';
+		if(this.props.data.fondeado){
+			projectClass += ' fondeado';
+		}
+		if(this.props.data.activo){
+			projectClass += ' activo';
+		}
+
 		return(
-			<div className="proyecto">
-			    <figure className="proyectoImg" style={{backgroundImage: 'url(img/proyectos/'+ this.props.data.image + ')'}} >
+			<div className={projectClass} >
+			    <figure className="proyectoImg" style={{backgroundImage: 'url(img/proyectos/'+ this.props.data.image + ')'}} onClick={this.projectClick}>
 			        <div className="hoverMask">Ver mas</div>
 			    </figure>
+
 			    <h4 className="proyectoTitle">{this.props.data.name}</h4>
 			    <p className="proyectoDescription">{this.props.data.description}</p>
+
 			    <div className="proyectoDetails">
 			        
 			        <div className="progresoMeta">
@@ -33,9 +47,10 @@ var Project = React.createClass({
 			        
 			        <div className="remainingDays">Dias Restantes <span className="days">37</span></div>
 			    </div>
+
 			    <div className="followSection">
 			        <div className="followers"> {this.props.data.followers} seguidores </div>
-			        <button onClick={this.followerManagement} className={this.props.data.following ? 'followBtn active' : 'followBtn'}> <span className="glyphicon glyphicon-star"></span></button>
+			        <button  className={this.props.data.following ? 'followBtn active' : 'followBtn'}> <span className="glyphicon glyphicon-star"></span></button>
 			    </div>
 			    
 			</div>
@@ -44,47 +59,132 @@ var Project = React.createClass({
 });
 
 var Projects = React.createClass({
-	getInitialState: function(){
-		return { proyects: this.props.data } 
-	},
-	followProject: function(index){
-		if (this.state.proyects[index].following) {
+	
 
-			this.state.proyects[index].following = false;
-			this.state.proyects[index].followers--;
-		}else{
-			this.state.proyects[index].following = true;
-			this.state.proyects[index].followers++;
-		}
-		
-	},
 	render: function(){
-		var followProject = this.followProject;
+		var showProjectInfo = this.props.showProjectInfo;
 		return (
 			<div className="proyectosContainer">
-				{ this.state.proyects.map(function(el, index){return <Project key={index} followProject={followProject} index={index} data={el}/> })}
+				{ this.props.data.map(function(el, index){return <Project key={el.id} onClick={showProjectInfo} index={index} data={el}/> })}
 			</div>
 		)
 	}
-})
+});
 
-var ProyectGroup = React.createClass({
+
+var ProjectGroup = React.createClass({
 	render: function(){
 		var sectionId = this.props.sectionIndex;
 		return (
-			<article className="seccionProyecto" id={this.props.sectionId}> >
+			<article className="seccionProyecto" id={this.props.sectionId}> 
 
 				<h3 className="proyectosTitle">
-                        <span className={this.props.iconClass} aria-hidden="true"></span> {this.props.sectionName}
+                    <span className={this.props.iconClass} aria-hidden="true"></span> {this.props.sectionName}
                 </h3>
-				<Projects data={ this.props.data.filter(function(el, index){return el.category == sectionId }) } />
+				<Projects showProjectInfo={this.props.showProjectInfo} data={this.props.projectsFiltered} />
 
+			</article>
+		)
+	}
+});
+
+
+var ProjectInfo = React.createClass({
+	componentDidMount: function() {
+	      console.log(this.props.name)
+	},
+	render: function(){
+		var iframeSRC = 'https://www.youtube.com/embed/' + this.props.video + '?autoplay=1';
+		return(
+			<article className={this.props.active ? 'barContainer visible' : 'barContainer hidden'}>
+				<iframe width="100%" height="40%" src={iframeSRC} frameborder="0" allowfullscreen></iframe>
+				<h3> {this.props.name} </h3>
+				<h6>Amigos siguiendo</h6>
+				<div className="amigos">
+					<figure className="amigo1">
+						<figcaption> Pedro Zamora</figcaption>
+					</figure>
+					<figure className="amigo2">
+						<figcaption> Pablo Ramirez</figcaption>
+					</figure>
+					<figure className="amigo3">
+						<figcaption> Toño Bravo</figcaption>
+					</figure>
+				</div>
+				<button className="verProyecto"> Ver Proyecto </button>
 			</article>
 		)
 	}
 })
 
+
+var ProjectsPanels = React.createClass({
+
+	getInitialState: function() {
+	    return {
+	        projects : this.props.data,
+	        showProjectInfo : this.showProjectInfo,
+	        projectActive : false,
+			activeProjectId : 0,
+			activeProjectData : this.props.data[0]
+	    };
+	},
+
+	showProjectInfo: function(projectId){
+		var projectSelected = this.state.projects.filter(function(el, index){return el.id == projectId });
+		this.setState({
+			projectActive : true,
+			activeProjectId : projectId,
+			activeProjectData : projectSelected[0]
+		})
+		
+		
+	},
+	render: function(){
+		
+		return(
+
+        	<div id="panels" className={this.state.projectActive ? 'secondaryPanelActive' : ''}>
+	        	<ProjectsNavBar />
+	        	<section className="layoutPanel primaryPanel" id="proyectsApp">
+
+        	    	<ProjectGroup  projectsFiltered={ this.state.projects.filter(function(el, index){return el.category == 0 }) } showProjectInfo={this.showProjectInfo} sectionId="proyectosNuevos" sectionName="Proyectos nuevos" iconClass="glyphicon glyphicon-fire"  />
+        	    	<ProjectGroup  projectsFiltered={ this.state.projects.filter(function(el, index){return el.category == 1 }) } showProjectInfo={this.showProjectInfo} sectionId="fondeadosSegunda" sectionName="Fondeados en 2da Ronda" iconClass="glyphicon glyphicon-ok"  />
+        	    	<ProjectGroup  projectsFiltered={ this.state.projects.filter(function(el, index){return el.category == 2 }) } showProjectInfo={this.showProjectInfo} sectionId="proyectosExitosos" sectionName="Proyectos exitosos" iconClass="glyphicon glyphicon-usd"  />
+        	    	<ProjectGroup  projectsFiltered={ this.state.projects.filter(function(el, index){return el.category == 3 }) } showProjectInfo={this.showProjectInfo} sectionId="proyectosFuturos" sectionName="Proyectos Futuros" iconClass="glyphicon glyphicon-flag"  />
+
+	        	</section>
+	        	<section className="layoutPanel secondaryPanel" data-53="top:109px; height: calc(100% - 109px);" data-54="top: 55px; height: calc(100% - 55px) ;">
+	        	    <ProjectInfo active={this.state.projectActive} name={this.state.activeProjectData.name} video={this.state.activeProjectData.video} goal={this.state.activeProjectData.goal} />
+	        	</section>
+        		
+        	</div>
+
+		);
+	}
+
+});
+
+var ProjectsNavBar = React.createClass({
+	render: function(){
+		return(
+				<nav id="proyectosNav" data-53="position:absolute;" data-54="position: fixed;">
+				    <h2 className="pageTitle pull-left">Proyectos</h2>
+				    <ul className="proyectosMenu pull-right">
+				        <li className="btn" data-section="proyectosNuevos"><span className="glyphicon glyphicon-fire" aria-hidden="true"></span> Proyectos nuevos</li>
+				        <li className="btn" data-section="fondeadosSegunda"><span className="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span> Fondeados en 2da Ronda</li>
+				        <li className="btn" data-section="proyectosExitosos"><span className="glyphicon glyphicon-usd" aria-hidden="true"></span> Proyectos exitosos</li>
+				        <li className="btn" data-section="proyectosFuturos"><span className="glyphicon glyphicon-flag" aria-hidden="true"></span> Proyectos Futuros</li>
+				    </ul>
+				</nav>
+			);
+	}
+});
+
+
 var ProjectsApp = React.createClass({
+
+
 	componentDidMount :function() {
 
 	   	// Listeners
@@ -101,7 +201,7 @@ var ProjectsApp = React.createClass({
 	   	}
 
 	   	function followProyect(e) {
-	   		$(this).toggleClass('active');;
+	   		$(this).toggleClass('active');
 	   	}
 
 	   	function scrollToSection(e) {
@@ -112,19 +212,18 @@ var ProjectsApp = React.createClass({
 	             scrollTop: targetSection.offset().top - 55
 	           }, 500);
 	   	}
+
+	   	var s = skrollr.init();
 	},
-  render: function(){
-        return(
-        	<div>
-        		<ProyectGroup  data={proyectsDB} sectionId="proyectosNuevos" sectionName="Proyectos nuevos" iconClass="glyphicon glyphicon-fire" sectionIndex="0" />
-        		<ProyectGroup  data={proyectsDB} sectionId="fondeadosSegunda" sectionName="Fondeados en 2da Ronda" iconClass="glyphicon glyphicon glyphicon-ok" sectionIndex="1" />
-        		<ProyectGroup  data={proyectsDB} sectionId="proyectosExitosos" sectionName="Proyectos exitosos" iconClass="glyphicon glyphicon-usd" sectionIndex="2" />
-        		<ProyectGroup  data={proyectsDB} sectionId="proyectosFuturos" sectionName="Proyectos Futuros" iconClass="glyphicon glyphicon-flag" sectionIndex="3" />
-        	</div>
-        	)
-  }
+
+	render: function(){
+	return(
+			<ProjectsPanels data={proyectsDB} />
+		)
+	}
+
 });
 
 
 
-ReactDOM.render(<ProjectsApp />, document.getElementById('proyectsApp'));
+ReactDOM.render(<ProjectsApp />, document.getElementById('mainApp'));
--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ var proyectsDB = [
 			name: 'Rekt4less',
 			description: 'Tienda online de videojuegos con un sistema de venta random, ya está funcionando y generando ventas, necesitamos inversión para crecer.',
 			currentMoney: 145000,
-			goal: 296600,
+			goal: 145000,
 			equity: 7,
 			firstTime:'Fri May 15 2016 14:25:16 GMT-0500 (Hora de verano central (México))', 
 			endTime: 'Fri Jun 15 2016 14:25:16 GMT-0500 (Hora de verano central (México))',
 			followers: 5,
 			category: 1,
-			fondeado: false,
+			fondeado: true,
 			image: 'proyectoImg1.jpg',
 			video: '-6C5TZKwn2Q',
 			following: true
@@ -29,7 +29,7 @@ var proyectsDB = [
 			category: 0,
 			fondeado: false,
 			image: 'proyectoImg1.jpg',
-			video: '-6C5TZKwn2Q',
+			video: 'fLBZFBS72XU',
 			following: false
 		},
 		{
@@ -65,7 +65,7 @@ var proyectsDB = [
 			following: false
 		},
 		{
-			id: 3,
+			id: 4,
 			name: 'Rekt4less',
 			description: 'Tienda online de videojuegos con un sistema de venta random, ya está funcionando y generando ventas, necesitamos inversión para crecer.',
 			currentMoney: 89000,
@@ -81,7 +81,7 @@ var proyectsDB = [
 			following: false
 		},
 		{
-			id: 4,
+			id:5,
 			name: 'Rekt4less',
 			description: 'Tienda online de videojuegos con un sistema de venta random, ya está funcionando y generando ventas, necesitamos inversión para crecer.',
 			currentMoney: 190000,
@@ -97,23 +97,23 @@ var proyectsDB = [
 			following: true
 		},
 		{
-			id: 5,
+			id: 6,
 			name: 'Rekt4less',
 			description: 'Tienda online de videojuegos con un sistema de venta random, ya está funcionando y generando ventas, necesitamos inversión para crecer.',
-			currentMoney: 150000,
-			goal: 296600,
+			currentMoney: 350000,
+			goal: 350000,
 			equity: 7,
 			firstTime:'Fri May 15 2016 14:25:16 GMT-0500 (Hora de verano central (México))', 
 			endTime: 'Fri Jun 15 2016 14:25:16 GMT-0500 (Hora de verano central (México))',
 			followers: 5,
 			category: 1,
-			fondeado: false,
+			fondeado: true,
 			image: 'proyectoImg1.jpg',
 			video: '-6C5TZKwn2Q',
 			following: false
 		},
 		{
-			id: 6,
+			id: 7,
 			name: 'Rekt4less',
 			description: 'Tienda online de videojuegos con un sistema de venta random, ya está funcionando y generando ventas, necesitamos inversión para crecer.',
 			currentMoney: 270000,
@@ -129,7 +129,7 @@ var proyectsDB = [
 			following: false
 		},
 		{
-			id: 3,
+			id: 8,
 			name: 'Rekt4less',
 			description: 'Tienda online de videojuegos con un sistema de venta random, ya está funcionando y generando ventas, necesitamos inversión para crecer.',
 			currentMoney: 1000,
@@ -150,4 +150,3 @@ var proyectsDB = [
 
 
 
-var s = skrollr.init();
